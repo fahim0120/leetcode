9faeebf1f27bedcf9e18fb4d4b03301f4d48f4f8
fahim0120 <fahim0120@gmail.com>
2020-05-12 16:15:15 -0400

    problem 101 iterative solution using single deque

--- 101 Symmetric Tree.py
+++ 101 Symmetric Tree.py
@@ -17,6 +17,43 @@
 #         self.right = right
 class Solution:
     def isSymmetric(self, root: TreeNode) -> bool:
+        from collections import deque
+
+        deq = deque()
+
+        if root:
+            deq.append(root.left)
+            deq.append(root.right)
+
+        while deq:
+            t1 = deq.popleft()
+            t2 = deq.popleft()
+
+            if not t1 and not t2:
+                continue
+
+            if not t1 or not t2:
+                return False
+
+            if t1.val != t2.val:
+                return False
+
+            deq.append(t1.left)
+            deq.append(t2.right)
+            deq.append(t1.right)
+            deq.append(t2.left)
+
+        return True
+
+'''
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSymmetric(self, root: TreeNode) -> bool:
 
         def helper(left: TreeNode, right: TreeNode) -> bool:
             if not left and not right:
@@ -33,3 +70,4 @@ class Solution:
             return True
 
         return helper(root.left, root.right)
+'''
